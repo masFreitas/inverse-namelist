cca0ff6e59d38efedf5fe29d6883bb31d77d8d85
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-30 19:09:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeiaEListeNomes__Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2) Faça um programa que leia 5 nomes e logo após, escreva os nomes na ordem inversa ao que foi digitada.
+            string[] nomes = new string[10];
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Escreva um nome:");
+                nomes[i] = Console.ReadLine();
+            }
+            
+            for (i = 5; i >= 0; i--)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+        }
+    }
+}
